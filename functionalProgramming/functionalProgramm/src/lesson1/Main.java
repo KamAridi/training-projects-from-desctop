2a969711205a63198711a8dbe7563b97d8f2aef1
KamAridi <aridi1994@mail.ru>
2022-11-11 22:15:59 +0300

    Completing first exercise

---
+++ functionalProgramming/functionalProgramm/src/lesson1/Main.java
@@ -0,0 +1,20 @@
+package lesson1;
+
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        Comparator<Integer> comparator = Integer::compare;
+
+        System.out.println(comparator.compare(1,10));
+
+    }
+
+
+}
+//class IntegerComparator implements Comparator<Integer>{
+//    @Override
+//    public int compare(Integer o1, Integer o2) {
+//        return Integer.compare(o1, o2);
+//    }
+//}
