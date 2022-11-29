b2906a32a4eef9f52b0e306aeb128a53718da4b8
KamAridi <aridi1994@mail.ru>
2022-11-29 19:49:03 +0300

    Finishing project reflection and annotation

---
+++ ReflectionAPIAndAnnotations/ReflectionAPIAndAnnotations/src/task1/Car.java
@@ -0,0 +1,30 @@
+package task1;
+
+@Table(name = "car")
+public class Car {
+    @Column("brand")
+    private String brand;
+    @Column("model")
+    private String model;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
---
+++ ReflectionAPIAndAnnotations/ReflectionAPIAndAnnotations/src/task1/CarRunner.java
@@ -0,0 +1,46 @@
+package task1;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class CarRunner {
+    public static void main(String[] args) {
+        Car car = new Car("Toyota", "Corolla");
+        System.out.println(car);
+        System.out.println(generateInsert(car));
+    }
+
+    private static String generateInsert(Car car){
+        String template = "INSERT INTO %s.%s (%s) VALUES (%S);";
+        Table table = car.getClass().getAnnotation(Table.class);
+        Field[] fields = car.getClass().getDeclaredFields();
+
+        String fieldNames = Arrays.stream(fields)
+                .filter(field -> field.isAnnotationPresent(Column.class))
+                .sorted(Comparator.comparing(field -> field.getName()))
+                .map(field -> field.getAnnotation(Column.class))
+                .map(Column::value)
+                .collect(Collectors.joining(", "));
+
+        String fieldValues = Arrays.stream(fields)
+                .filter(field -> field.isAnnotationPresent(Column.class))
+                .sorted(Comparator.comparing(Field::getName))
+                .peek(field -> field.setAccessible(true))
+                .map(field -> {
+                    try {
+                        return String.valueOf(field.get(car));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                        return "";
+                    }
+                })
+                .map(value -> "'" + value + "'")
+                .collect(Collectors.joining(", "));
+        return String.format(template, table.schema(),
+                table.name(), fieldNames, fieldValues);
+
+
+    }
+}
